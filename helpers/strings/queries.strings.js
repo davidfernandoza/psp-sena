f762944ba95b6546981702adf262bd130ea5eee6
davidfernandoza <fernando-zapata@live.com>
2020-07-15 20:05:23 -0500

    Se arregla error en consulta de organizacion

--- helpers/strings/queries.strings.js
+++ helpers/strings/queries.strings.js
@@ -8,7 +8,7 @@ class QueriesString {
 		}
 		this.ProgramsRepository = {
 			getAllByOrganization:
-				'SELECT DISTINCT p.id, p.users_id, p.languages_id, p.modules_id, p.name, p.description, p.total_lines, p.planning_date, p.start_date, p.update_date, p.delivery_date, p.created_at, p.updated_at FROM organizations AS o INNER JOIN users AS u ON o.id = u.organizations_id INNER JOIN projects_users AS pu ON pu.users_id = u.id INNER JOIN projects AS pj ON pu.projects_id = pj.id INNER JOIN modules AS m ON m.projects_id = pj.id INNER JOIN programs AS p ON p.modules_id = m.id WHERE o.id = :idOrganization;'
+				'SELECT DISTINCT p.id, p.users_id, p.languages_id, p.modules_id, p.name, p.description, p.total_lines, p.planning_date, p.start_date, p.delivery_date, p.created_at, p.updated_at FROM organizations AS o INNER JOIN users AS u ON o.id = u.organizations_id INNER JOIN projects_users AS pu ON pu.users_id = u.id INNER JOIN projects AS pj ON pu.projects_id = pj.id INNER JOIN modules AS m ON m.projects_id = pj.id INNER JOIN programs AS p ON p.modules_id = m.id WHERE o.id = :idOrganization;'
 		}
 	}
 }
