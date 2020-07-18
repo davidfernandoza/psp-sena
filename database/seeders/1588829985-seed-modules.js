02086caaa760268bc4adf6572ffc549f74db0559
davidfernandoza <fernando-zapata@live.com>
2020-07-17 20:43:10 -0500

    Se cambia la disposicion de las fechas de unix en segundos a unix en milisegundos

--- database/seeders/1588829985-seed-modules.js
+++ database/seeders/1588829985-seed-modules.js
@@ -11,8 +11,8 @@ module.exports = {
 					name: 'Modulo 1',
 					description:
 						'Modulo 1 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-					planning_date: 1577883600,
-					start_date: 1577883600,
+					planning_date: 1577883600 * 1000,
+					start_date: 1577883600 * 1000,
 					finish_date: null,
 					created_at: new Date().toDateString(),
 					updated_at: new Date().toDateString()
@@ -22,8 +22,8 @@ module.exports = {
 					name: 'Modulo 2',
 					description:
 						'Modulo 2 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-					planning_date: 1580734800,
-					start_date: 1580734800,
+					planning_date: 1580734800 * 1000,
+					start_date: 1580734800 * 1000,
 					finish_date: null,
 					created_at: new Date().toDateString(),
 					updated_at: new Date().toDateString()
